9558968d69622373aff0f4ee3fff3adef89fc158
ranyl666 <138106818+ranyl666@users.noreply.github.com>
2023-06-29 18:10:06 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/test-img.jpg') {
+      myImage.setAttribute('src', 'images/test-2.jpeg');
+    } else {
+      myImage.setAttribute('src', 'images/test-img.jpg');
+    }
+}
+
+  let myButton = document.querySelector('button');
+  let myHeading = document.querySelector('h1');
+
+ function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+   }
+
+   if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+ }
+ function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+    }
+  }
